6cd4b4471ee94b8f8c32a6e5e22db955bcd590b9
leomols97 <leomols@hotmail.com>
2022-02-24 13:33:09 +0100

    Beginning Thymeleaf

--- Chap5-Spring/hello/src/main/java/webg5/spring/hello/HomeController.java
+++ Chap5-Spring/hello/src/main/java/webg5/spring/hello/HomeController.java
@@ -1,13 +1,13 @@
 package webg5.spring.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller public class HomeController
-{
-    @GetMapping ("/")
-    public String home ()
-    {
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
         return "home";
     }
 }
