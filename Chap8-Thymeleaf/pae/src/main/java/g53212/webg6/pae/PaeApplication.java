1c8954a1c8d56e1db12c2a873fc6633ae5987772
leomols97 <leomols@hotmail.com>
2022-02-24 14:19:50 +0100

    Trying to deploy on Heroku

---
+++ Chap8-Thymeleaf/pae/src/main/java/g53212/webg6/pae/PaeApplication.java
@@ -0,0 +1,13 @@
+package g53212.webg6.pae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaeApplication.class, args);
+	}
+
+}
